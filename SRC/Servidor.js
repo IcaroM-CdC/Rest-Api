3366660a02e7a63ff8867de9e3e1584c1ef05bd2
IcaroM-CdC <icaro.moreira@protonmail.com>
2020-02-25 23:00:19 -0300

    Pre banco de dados

--- SRC/Servidor.js
+++ SRC/Servidor.js
@@ -1,15 +1,42 @@
 const Porta = 3003
 
 const Express = require("express")
+const BodyParser = require("body-parser")
+const RotaProdutos = require("./Rotas/Produtos")
+const RotaPedidos = require("./Rotas/Pedidos")
+
 const APP = Express()
 
+//Estudar sobre CORS que ainda não fiz na API
 
-APP.get("/", function(Requisicao, Resposta){
+APP.use(BodyParser.urlencoded({ extended: false }))
+APP.use(BodyParser.json()) //SO VAI ACEITAR DADOS JSON COMO ENTRADA NO BODY
+APP.use("/produtos", RotaProdutos)
+APP.use("/pedidos", RotaPedidos)
 
-    Resposta.send("Teste")
+// TRATANDO O ERRO CASO UMA ROTA NÃO DEFINIDA FOR INFORMADA
+
+APP.use(function(Requisicao, Resposta, Next){
+
+    const Erro = new Error("Não encontrado!!!")
+    Erro.status = 404
+    Next(Erro)
 
 })
 
+APP.use(function(Erro, Requisicao, Resposta, Next) {
+
+    Resposta.status(Erro.status || 500)
+    return Resposta.send({
+
+        Erro: {
+            Mensagem: Erro.message
+        }
+
+    })
+})
+
+// # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 APP.listen(Porta, function(){
 
