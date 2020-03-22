fb4fe903ea115086befe66338accb80e0d2ca2fa
IcaroM-CdC <icaro.moreira@protonmail.com>
2020-03-22 16:49:41 -0300

    Tratando os pedidos

--- SRC/Rotas/Produtos.js
+++ SRC/Rotas/Produtos.js
@@ -2,38 +2,15 @@ const Express = require("express")
 const MySQL = require("../MySQL").Pool
 const Router = Express.Router()
 
-Router.get("/", function(Requisicao, Resposta, Next){
-
-    Resposta.status(200).send({
-
-        Mensagem: "Usando o GET dentro da rota de produtos"
-
-    })
-})
-
-Router.get("/:ID_Produto", function(Requisicao, Resposta, Next){
-
-    Resposta.status(200).send({
-
-        Mensagem: "Retornando um produto exclusivo"
-
-    })
-})
+// ADICIONA UM NOVO PRODUTO
 
 Router.post("/", function(Requisicao, Resposta, Next){
 
     // COMANDO MYSQL PARA INTRODUZIR O PRODUTO NO BANCO DE DADOS
     MySQL.getConnection(function(Erro, Conexao) {
 
-
-        console.error(Erro)
+        if (Erro){  return Resposta.status(500).send({error: Erro})  }
         
-        if (Erro == null) {
-            console.log("DEU CERTO")
-        }
-        else{
-            console.error("FUDEU")
-        }
         Conexao.query(
 
             "INSERT INTO Produtos (Nome, Preco) VALUES (?,?)",
@@ -49,32 +26,198 @@ Router.post("/", function(Requisicao, Resposta, Next){
                         Response: null
                     })
                 }
+
+                const Resposta = {
+                    Mensagem: "Produto criado com sucesso",
+                    Produto: {
+                        ID_Produto: Resultado.ID_Produto,
+                        Nome: Requisicao.body.Nome,
+                        Preco: Requisicao.body.Preco,
+                        Requisicao: {
+                            Tipo: "POST",
+                            Descricao: "Insere um produto no banco de dados",
+                            URL: "http://localhost:3003/produtos"
+                        }
+                    }
+                }
                 
-                Resposta.status(201).send({
-                    Mensagem: "Produto criado com sucesso!!",
-                    ID_Produto: Resultado.insertId
-                })
+                return Resposta.status(201).send(Resposta)
             }
         )
     })
 })
 
+//RETORNA TODOS OS PRODUTOS
 
-Router.patch("/:ID_Produto", function(Requisicao, Resposta, Next){
+Router.get("/", function(Requisicao, Resposta, Next){
 
-    Resposta.status(201).send({
+    MySQL.getConnection(function(Erro, Conexao){
 
-        Mensagem: "Usando o PATCH dentro da rota de produtos"
+        if (Erro){  return Resposta.status(500).send({error: Erro})  }
+
+        Conexao.query(
+
+            "SELECT * FROM Produtos;",
+
+            function(Erro, Resultado, Field){
+                if (Erro){  return Resposta.status(500).send({error: Erro})  }
+                
+                // DETALHANDO A RESPOSTA
+                const Resposta = {
+                    Quantidade: Resultado.length,
+                    Produtos: Resultado.map(Prod => {
+                        return {
+                            ID_Produto: Prod.ID_Produto,
+                            Nome: Prod.Nome,
+                            Preco: Prod.Preco,
+                            Requisicao: {
+                                Tipo: "GET",
+                                Descricao: "Retornando Todos os produtos do banco de dados",
+                                URL: "http://localhost:3003/produtos"
+                            }
+                        }
+                    })
+                }
+
+                return Resposta.status(200).send({Resposta})
+                
+            }
+        )
 
     })
 })
 
+// RETORNA OS DADOS DE UM PRODUTO
+
+Router.get("/:ID_Produto", function(Requisicao, Resposta, Next){
+
+    MySQL.getConnection(function(Erro, Conexao){
+
+        if (Erro){  return Resposta.status(500).send({error: Erro})  }
+
+        Conexao.query(
+
+            "SELECT * FROM Produtos WHERE ID_Produto = ?;",
+            [Requisicao.params.ID_Produto],
+
+            function(Erro, Resultado, Field){
+                if (Erro){  return Resposta.status(500).send({error: Erro})  }
+
+                // VERIFICA CASO O ID FORNECIDO SEJA INVALIDO
+                if (Resultado.lenght == 0){
+                    return Resposta.status(404).send({
+                        Mensagem: "Não foi encontrado o produto com esse ID"
+                    })
+                }
+                
+                const Resposta = {
+                    Produto: {
+                        ID_Produto: Requisicao.body.ID_Produto,
+                        Nome: Requisicao.body.Nome,
+                        Preco: Requisicao.body.Preco,
+                        Requisicao: {
+                            Tipo: "GET",
+                            Descricao: "Retorna os dados de um produto",
+                            URL: "http://localhost:3003/produtos/" + Requisicao.body.ID_Produto
+                        }
+                    }
+                }
+                
+                return Resposta.status(200).send(Resposta)
+                
+            }
+        )
+
+    })
+
+})
+
+// ATUALIZA UM PRODUTO
+
+Router.patch("/:ID_Produto", function(Requisicao, Resposta, Next){
+
+    MySQL.getConnection(function(Erro, Conexao) {
+
+        if (Erro){  return Resposta.status(500).send({error: Erro})  }
+        
+        Conexao.query(
+
+            "UPDATE Produtos SET Nome = ?, Preco = ? WHERE ID_Produtos = ?",
+            [Requisicao.body.Nome, Requisicao.body.Preco, Requisicao.body.ID_Produto],
+            
+            //CALLBACK PARA ENCERRAR A CONEXÃO DEPOIS DO TERMINO DA REQUISIÇÃO
+            function(Erro, Resultado, Field){
+                Conexao.release()
+
+                if (Erro) {
+                    return Resposta.status(500).send({
+                        Erro: Erro,
+                        Response: null
+                    })
+                }
+
+                const Resposta = {
+                    Mensagem: "Produto atualizado com sucesso",
+                    Produto: {
+                        ID_Produto: Requisicao.body.ID_Produto,
+                        Nome: Requisicao.body.Nome,
+                        Preco: Requisicao.body.Preco,
+                        Requisicao: {
+                            Tipo: "PATCH",
+                            Descricao: "Atualiza os dados de um produto",
+                            URL: "http://localhost:3003/produtos/" + Requisicao.body.ID_Produto
+                        }
+                    }
+                }
+                
+                return Resposta.status(202).send(Resposta)
+
+            }
+        )
+    })
+
+})
+
+// DELETA UM PRODUTO
+
 Router.delete("/:ID_Produto", function(Requisicao, Resposta, Next){
 
-    Resposta.status(201).send({
+    MySQL.getConnection(function(Erro, Conexao) {
 
-        Mensagem: "Usando o DELETE dentro da rota de produtos"
+        if (Erro){  return Resposta.status(500).send({error: Erro})  }
+        
+        Conexao.query(
 
+            "DELETE FROM Produtos WHERE ID_Produto = ?",
+            [Requisicao.body.ID_Produto],
+            
+            //CALLBACK PARA ENCERRAR A CONEXÃO DEPOIS DO TERMINO DA REQUISIÇÃO
+            function(Erro, Resultado, Field){
+                Conexao.release()
+
+                if (Erro) {
+                    return Resposta.status(500).send({
+                        Erro: Erro,
+                        Response: null
+                    })
+                }
+
+                const Resposta = {
+                    Mensagem: "Produto removido com sucesso",
+                    Requisicao: {
+                        Tipo: "Delete",
+                        Descricao: "Remove um produto",
+                        URL: "http://localhost:3003/produtos",
+                        Body: {
+                            Nome: "String",
+                            Preco: "Number"
+                        }
+                    }
+                }
+                
+                return Resposta.status(202).send(Resposta)
+            }
+        )
     })
 })
 
